58673f6eb099b7924a9aed1b1be1da5ce4f04f07
mrsimb <pavlow94@gmail.com>
2017-02-05 18:32:44 +0300

    Description update

--- MacroRecorder.lua
+++ MacroRecorder.lua
@@ -2,48 +2,48 @@
 ------------------------------------ README ------------------------------------
 --------------------------------------------------------------------------------
 --[[
-  MacroRecorder v0.1 beta
-  https://github.com/mrsimb/EasyHotkey
+Script for LuaMacros that let you record macros without programming required.
 
-  LuaMacros
-  https://github.com/me2d13/luamacros
+Download link:
+https://github.com/mrsimb/macrorecorder/archive/master.zip
 
-  TODO:
-  - Saving config (for now, all recorded macros will be deleted upon app exit)
+GitHub:
+https://github.com/mrsimb/macrorecorder
 
-  Default key for recording macro is 19 (PAUSE/BREAK).
+TODO:
+- Saving config (for now, all recorded macros will be deleted upon app exit)
 
-  By default, this script will automaticaly try to use ALL
-  connected keyboards and let you SET THEM UP.
+Default key for recording macro is 45 (INSERT).
 
-  In that period, all keyboards will TRY to behave normally,
-  but due to LUASCRIPT LIMITATIONS, you wouldn't be able to:
-  - hold TAB, SHIFT, CTRL, ALT keys
+By default, this script will automaticaly try to use ALL connected keyboards and let you SET THEM UP.
 
-  To change recording hotkey, do following:
-  1. Go to "KEYNAMES SECTION"
-  2. Find desired button
-  3. Copy its number
-  4. Paste it instead of default macroHotkey number in "SETTINGS SECTION"
-  look at keyNames table in "KEYNAMES SECTION" and find correct number.
+In that period, all keyboards will TRY to behave normally, but due to LUASCRIPT LIMITATIONS, you wouldn't be able to:
+- hold TAB, SHIFT, CTRL, ALT keys (they will be sent repeatly)
 
-  To record macro, do following:
-  1. Press and hold your hotkey combination (for example, "c" or "ctrl+shift+a")
-  2. Press macro hotkey (do it quickly!)
-  3. Release all keys
-  4. Type desired key sequence (for example, "ctrl+a, ctrl+c, right, ctrl+v")
-  5. Press macro hotkey again
+To change recording hotkey, do following:
+1. Go to "KEYNAMES SECTION"
+2. Find desired button
+3. Copy its number
+4. Paste it instead of default macroHotkey number in "SETTINGS SECTION"
+look at keyNames table in "KEYNAMES SECTION" and find correct number.
 
-  To delete macro, do following:
-  1. Press and hold your hotkey combination
-  2. Release all keys
-  3. Press macro hotkey again, without typing any sequence
+To record macro, do following:
+1. Press and hold your hotkey combination (for example, "c" or "ctrl+shift+a")
+2. Press macro hotkey (do it quickly!)
+3. Release all keys
+4. Type desired key sequence (for example, "ctrl+a, ctrl+c, right, ctrl+v")
+5. Press macro hotkey again
 
-  AFTER you set your up hotkey combinations, do following:
-  1. Go to "SETTINGS SECTION"
-  2. Comment or delete "use('all')" line
-  3. Type "use('your_customized_keyboard_id')"
-     ID of triggered keyboard is shown in log form below, when you press any key
+To delete macro, do following:
+1. Press and hold your hotkey combination
+2. Release all keys
+3. Press macro hotkey again, without typing any sequence
+
+AFTER you set up your hotkey combinations, do following:
+1. Go to "SETTINGS SECTION"
+2. Comment or delete "use('all')" line
+3. Type "use('your_customized_keyboard_id')"
+ID of triggered keyboard is shown in log form below, when you press any key.
 --]]
 
 --------------------------------------------------------------------------------
